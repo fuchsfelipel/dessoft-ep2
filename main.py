2321f11940c2aec0c79c594a4fef7e4003c3b1d1
Felipe Liberman Fuchs <felipe@almosthackers.net>
2021-05-03 15:20:24 -0300

    Fix file open

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from tools import * # Importa todas as funções básicas para o jogo
 import time
+import os
+import sys
+
 # Imprimir a mensagem de inicio de jogo e esperar enter
-with open("message.txt", 'r') as f:
+with open(os.path.join(sys.path[0], "message.txt"), 'r') as f:
     print(f.read())
 
 input("\nAperte [Enter] para iniciar o jogo...\n")
