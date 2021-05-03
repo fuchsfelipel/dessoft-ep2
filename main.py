8a9917b8792734d027294656cdac20f0e9ec3c1f
Felipe Liberman Fuchs <felipe@almosthackers.net>
2021-05-03 14:07:49 -0300

    Remove gameflow

--- main.py
+++ main.py
@@ -9,21 +9,4 @@ input("\nAperte [Enter] para iniciar o jogo...\n")
 # Gerar e Mostrar Baralho
 baralho = cria_baralho()
 
-# Define uma jogada
-def jogar():
-    statusJogo = True
-    
-    # Mostrar info do baralho p usuário
-    print("Status atual do jogo:\n--------\n")
-    print(mostra_baralho(baralho))
 
-    baralho_atual = baralho
-    while statusJogo:
-        index = int(input(f"Digite a carta que deseja mover (1 - {len(baralho)})")) - 1
-        statusJogo, baralho_atual = fazJogada(baralho_atual, index)
-
-# Jogar
-while True:
-    jogar()
-    if (input("Você gostaria de jogar novamente (s/n)?") == 'n'):
-        break 
